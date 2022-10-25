ff31e2fa1919d006483ed73e062d51c3f0ac9beb
IITS\se19okk <s196278@stud.uni-ruse.bg>
2022-10-25 13:07:41 +0300

    Добавих index.php и contacts.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Здравей :)
+    </body>
+<a href= "H:\2022-23\Cloud computing\azure_php\contacts.php">Kontakti </a>
+</html>
